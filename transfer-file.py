5ff296a36be2002ba3521dbbacb6be16fd285115
Rayan <108743715+rayanlevrai@users.noreply.github.com>
2023-04-15 03:50:02 +0200

    Update and rename app-transfer-size.py to transfer-file.py

--- transfer-file.py
+++ transfer-file.py
@@ -14,11 +14,10 @@ def upload_file():
     
     # Configure the SFTP connection with an SSH certificate
     cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None # Ignorer les erreurs d'authentification    
+    cnopts.hostkeys = None # Ignore authentication errors    
     with pysftp.Connection('IP.OF.YOUR.SERVER', username='USERNAME', password='PASSWORD', cnopts=cnopts) as sftp:
 
     # If you would to use a ssh certificat for logging, replace line 18 for this :
-    
     # private_key_path = "path/of/your/private/ssh.key"
     # with pysftp.Connection('IP.OF.YOUR.SERVER', username='USERNAME', private_key=private_key_path, cnopts=cnopts) as sftp:
   
@@ -60,7 +59,7 @@ remote_path_entry.pack(side=tk.LEFT)
 
 remote_path_entry.insert(0, "/var/www/html/") # by default
 
-# Champ pour le lien généré
+# Fields for link generated
 file_url_frame = tk.Frame(root)
 file_url_frame.pack(pady=10)
 
@@ -70,7 +69,7 @@ file_url_label.pack(side=tk.LEFT)
 file_url_entry = tk.Entry(file_url_frame, width=40)
 file_url_entry.pack(side=tk.LEFT)
 
-# Bouton pour uploader le fichier
+# Button for upload file
 upload_button = tk.Button(root, text="Upload your file", command=upload_file)
 upload_button.pack(pady=10)
 
